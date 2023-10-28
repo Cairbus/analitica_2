508d333700d946486a4422bd2c89e044b63420a6
Cairbus <145635831+Cairbus@users.noreply.github.com>
2023-10-28 12:08:00 -0500

    pagina.py

--- pagina.py
+++ pagina.py
@@ -84,7 +84,7 @@ if selected == "Datos históricos de accidentalidad":
     display_map(df_filtered, month, tipo_choque)
 
 elif selected=="¿Cómo usarlo?":
-    st.video("https://www.youtube.com/watch?v=UlQwcvwNMvo&pp=ygUcYWNjaWRlbnRlcyBlbiBtZWRlbGxpbiBkYXRvcw%3D%3D")
+    st.video("https://youtu.be/U6dW1nEns9I?si=46kiS9qpsUbNmVe2")
     st.subheader("También puedes encontrar el informe y el proceso de creacion del modelo en el siguiente repositorio de GitHub https://github.com/alejozq/Trabajo-2-Analitica")
     
 elif selected=="Predicción":
